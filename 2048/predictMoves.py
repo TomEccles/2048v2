ed2cb6045bd03483fc8d9a102682ee0c48676603
Tom Eccles <None>
2017-05-05 07:47:48 +0100

    Updates to the trainer to save a model.
    This does bit better than using only information about which moves are legal.

--- 2048/predictMoves.py
+++ 2048/predictMoves.py
@@ -12,51 +12,7 @@ def accuracy_matrix(pred, labels):
             print("%.3f" % (matches/total),end=",")
         print()
 
-accuracy_matrix([[0,0,1,0],[0,0,0,1],[1,0,0,0],[1,0,0,0],[0,0,0,1]], [[0,0,1,0],[0,0,1,0],[0,1,0,0],[1,0,0,0],[0,0,0,1]])
 
-
-file = 'moves.txt'
-num_labels = 4
-print(math.log(3))
-
-with open(file, 'rb') as f:
-  arr = np.loadtxt(f, delimiter=',')
-
-positions = arr[:,:-1]
-print(positions.shape)
-print(positions[100])
-
-moves = arr[:,-1]
-moves = (np.arange(num_labels) == moves[:,None]).astype(np.float32)
-
-batch_size = 128
-nodes_1 = 512
-nodes_2 = 128
-nodes_3 = 32
-num_inputs = positions.shape[2]
-num_outputs = 4
-
-
-valid_p = positions[-10000:,:].astype(np.float32)
-valid_m = moves[-10000:,:].astype(np.float32)
-positions = positions[:-10000,:].astype(np.float32)
-moves = moves[:-10000,:].astype(np.float32)
-
-print(positions[0])
-print(moves[0])
-print(positions.shape)
-print(moves.shape)
-
-
-l=0
-for i in range(4):
-    print(i, ": ", sum(valid_m[:,i]))
-    prop = sum(valid_m[:,i]) / valid_m.shape[0]
-    print (prop)
-    l += -prop * math.log(prop)
-print("Naive loss on validation set: ", l)
-
-entropy_graph = tf.Graph()
 
 def permute(dataset, labels):
   permutation = np.random.permutation(dataset.shape[0])
@@ -64,8 +20,44 @@ def permute(dataset, labels):
   shuffled_labels = labels[permutation,:]
   return shuffled_dataset, shuffled_labels
 
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
+file = 'moves2.txt'
+with open(file, 'rb') as f:
+  arr = np.loadtxt(f, delimiter=',')
+
+
+positions = arr[:,:-1]
+
+num_labels = 4
+moves = arr[:,-1]
+moves = (np.arange(num_labels) == moves[:,None]).astype(np.float32)
+
+batch_size = 128
+nodes_1 = 512
+nodes_2 = 128
+nodes_3 = 32
+num_inputs = positions.shape[1]
+num_outputs = num_labels
+
+valid_p = positions[:10000,:].astype(np.float32)
+valid_m = moves[:10000,:].astype(np.float32)
+
+positions = positions[10000:,:].astype(np.float32)
+moves = moves[10000:,:].astype(np.float32)
+
 permute(positions, moves)
 
+prop = [sum(valid_m[:,i]) / valid_m.shape[0] for i in range(4)]
+guesses = [(i[-4:] * prop) / sum(i[-4:]*prop)  for i in valid_p]
+fixed_guesses = [np.where(i > 0, i, 1) for i in guesses]
+badness = [sum(-valid_m[i] * [math.log(x) for x in fixed_guesses[i]]) for i in range(10000)]
+print("Loss after knowing legality: ", np.mean(badness))
+
+
+entropy_graph = tf.Graph()
 def calc(x, keep_param = 1):
   mat_1 = tf.matmul(x, weights_1) + biases_1
   rel_1 = tf.nn.relu(mat_1)
@@ -123,38 +115,38 @@ with entropy_graph.as_default():
   forwards_input = tf.placeholder(tf.float32, shape=(None, num_inputs))
   forwards_pred = calc(forwards_input)
 
-def accuracy(predictions, labels):
-  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
-          / predictions.shape[0])
-
-num_steps = 30001
-reg=0.03
-lr = 0.03
-for dropout in [1]:
-    best = 0
-    with tf.Session(graph=entropy_graph) as session:
-      tf.initialize_all_variables().run()
-      print("Initialized [reg, drop, learn]", reg, dropout, lr)
-      for step in range(num_steps):
-        # Pick an offset within the training data, which has been randomized.
-        # Note: we could use better randomization across epochs.
-        offset = (step * batch_size) % (moves.shape[0] - batch_size)
-        # Generate a minibatch.
-        batch_data = positions[offset:(offset + batch_size), :]
-        batch_labels = moves[offset:(offset + batch_size), :]
-        # Prepare a dictionary telling the session where to feed the minibatch.
-        # The key of the dictionary is the placeholder node of the graph to be fed,
-        # and the value is the numpy array to feed to it.
-        feed_dict = {X : batch_data, Y : batch_labels, reg_param : reg, keep_param : dropout, learning_rate : lr}
-        _, l = session.run( [optimizer, loss], feed_dict=feed_dict )
-        if (step % 1000 == 0):
-         print("step %i loss: %f" % (step,l), end=",")
-        if (step % 5000 == 0):
-          print()
-          l, pred = session.run( [valid_loss, valid_pred] )
-          a = accuracy(pred, valid_m)
-          print("Validation accuracy: %.1f%%" % a)
-          print("Validation loss: %.4f" % l)
-          accuracy_matrix(pred, valid_m)
+num_steps = 5000001
+dropout = 0.2
+reg = 0
+best = 10000
+with tf.Session(graph=entropy_graph) as session:
+  tf.initialize_all_variables().run()
+  tf.train.Saver().save(session,"./predictMoves.ckpt")
+  print("Initialized [reg, drop]", reg, dropout)
+  for step in range(num_steps):
+    # Pick an offset within the training data, which has been randomized.
+    # Note: we could use better randomization across epochs.
+    offset = (step * batch_size) % (moves.shape[0] - batch_size)
+    # Generate a minibatch.
+    batch_data = positions[offset:(offset + batch_size), :]
+    batch_labels = moves[offset:(offset + batch_size), :]
+    # Prepare a dictionary telling the session where to feed the minibatch.
+    # The key of the dictionary is the placeholder node of the graph to be fed,
+    # and the value is the numpy array to feed to it.
+    feed_dict = {X : batch_data, Y : batch_labels, reg_param : reg, keep_param : 1-dropout}
+    _, l = session.run( [optimizer, loss], feed_dict=feed_dict )
+    if (step % 1000 == 0):
+     print("step %i loss: %f" % (step,l), end=",")
+    if (step % 10000 == 0):
+      print()
+      l, pred = session.run( [valid_loss, valid_pred] )
+      a = accuracy(pred, valid_m)
+      print("Validation accuracy: %.1f%%" % a)
+      print("Validation loss: %.4f" % l)
+      accuracy_matrix(pred, valid_m)
+      if l > best:
+          tf.train.Saver().save(session, "./predictMoves.ckpt")
+          break
+      else: best = l
           
           
